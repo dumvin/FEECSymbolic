36304c190dd4681a8f2248cb2171799c6b2ef45b
dumvin <dumvin@users.noreply.github.com>
2014-03-27 13:28:48 +0000

    Working version of preorder visitor
    Working version of the preorder visitor. the print could be evolved in just a line, and the different versions of visitor (inorder, post order) could be written)

--- visitor.py
+++ visitor.py
@@ -1,11 +1,12 @@
-from complex import *
-from form import *
+import complex 
+import form as Form
 #We define the different function that needs to be done by our Visitor on the nodes
 def func_term_fo(form):
     """function that print a terminal Form"""
     if form.scalar == 1:
-        return form.name
-    return "{}{} ".format(form.scalar,form.name)  
+        print( form.name)
+    else:
+        print( "{}{} ".format(form.scalar,form.name)  )
 
 dic_term_fo = {}    
 
@@ -44,32 +45,35 @@ def hod(form):
     #return "* "
     print("* ")
     
-dic_op_fo = {D: (diff, {}), Wedge: (wed, {}), Add: (add,{}), Hodge: (hod, {}) }
+dic_op_fo = {Form.D: (diff, {}), Form.Wedge: (wed, {}), Form.Add: (add,{}), Form.Hodge: (hod, {}) }
 
 class Visitor(object):
     """this class implement a tree visitor for our form"""
     def __init__(self):
-        self.handler_dict = {OperatorForm:(func_op_fo,dic_op_fo),TerminalForm:(func_term_fo,dic_term_fo)}
+        self.handler_dict = {Form.OperatorForm:(func_op_fo,dic_op_fo),Form.TerminalForm:(func_term_fo,dic_term_fo)}
 
     def visit(self, form):
         """for the moment we will do a preorder """
-        if not isinstance(form,TerminalForm):
+        if not isinstance(form,Form.TerminalForm):
             children=form.forms
             self.find_method(form)(form)
             for fo in children:
                 self.visit(fo)
             #in post order the find_method will be there
-        self.find_method(form)(form)
+        else:
+            self.find_method(form)(form)
         
     def find_method(self, form, H=None, D=None):
         """This method enables to choose the method that we wamt to apply to a specific node"""
         if D is None:
             D = self.handler_dict
+           
         for typ, val in D.items():
+             
             #m = __import__("form")
             #m = getattr( m, typ)
             #I do not know if there is a nicer way to what i just did
             if isinstance(form, typ):
-                print(typ)
-                return self.find_method(form, val) or H
-            return(H)
+                
+                return (self.find_method(form, *val)) #or H
+        return(H)
